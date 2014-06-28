7110d4c6aba409a659e63f0c92f99dd9f68ece4f
Kerry Buckley <kerryjbuckley@gmail.com>
2014-06-28 11:25:37 +0100

    Make the first test pass

--- Stack.java
+++ Stack.java
@@ -1,12 +1,12 @@
 class Stack {
   public boolean isEmpty() {
-    return false;
+    return true;
   }
 
   public static void main(String[] args) {
     Stack stack = new Stack();
     if (!stack.isEmpty()) {
-      System.err.println("Expected a new stack to be empty");
+      System.err.println("A new stack should be empty");
     }
   }
 }
