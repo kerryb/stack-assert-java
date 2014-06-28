ad027e84dfb337321eb3122c428cebd608264fe8
Kerry Buckley <kerryjbuckley@gmail.com>
2014-06-28 11:33:54 +0100

    Extract assertTrue and assertFalse methods

--- Stack.java
+++ Stack.java
@@ -9,16 +9,24 @@ class Stack {
     return empty;
   }
 
+  private static void assertTrue(boolean actual, String message) {
+    if (!actual) {
+      System.err.println(message);
+    }
+  }
+
+  private static void assertFalse(boolean actual, String message) {
+    if (actual) {
+      System.err.println(message);
+    }
+  }
+
   public static void main(String[] args) {
     Stack stack = new Stack();
-    if (!stack.isEmpty()) {
-      System.err.println("A new stack should be empty");
-    }
+    assertTrue(stack.isEmpty(), "A new stack should be empty");
 
     stack = new Stack();
     stack.push("foo");
-    if (stack.isEmpty()) {
-      System.err.println("A stack that's been pushed to should not be empty");
-    }
+    assertFalse(stack.isEmpty(), "A stack that's been pushed to should not be empty");
   }
 }
